51f5c521af2b338b7286b07ea798cc02bdcf03d7
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-07-17 00:31:53 +0900

    an experiment for component forwarder

---
+++ BehaviourForwarder/exprimental/SampleLibrary/TestHueBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SampleLibrary
+{
+    class TestHueBehaviour : MonoBehaviour
+    {
+        /// <summary>
+        /// 1フレームあたりどのくらい色相角が変わるか。
+        /// 色相角の範囲は[0, 1]
+        /// </summary>
+        [SerializeField]
+        float _speed = 0;
+
+        private Text _text;
+        private float _hue;
+        private bool _increase;
+
+        void Start()
+        {
+            _text = GetComponent<Text>();
+        }
+
+        void Update()
+        {
+            if (_increase)
+            {
+                _hue += _speed;
+                if (_hue > 1)
+                {
+                    _hue = 1;
+                    _increase = false;
+                }
+            }
+            else
+            {
+                _hue -= _speed;
+                if (_hue < 0)
+                {
+                    _hue = 0;
+                    _increase = transform;
+                }
+            }
+
+            var hsv = new Colorspace.ColorHSV(_hue, 1, 1);
+            var rgb = new Colorspace.ColorRGB(hsv);
+            _text.color = new Color((float)rgb.R, (float)rgb.G, (float)rgb.B, 1);
+        }
+    }
+}
---
+++ BehaviourForwarder/exprimental/SampleLibrary/TextBrightnessBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SampleLibrary
+{
+    [RequireComponent(typeof(Text))]
+    public class TextBrightnessBehaviour : MonoBehaviour
+    {
+        /// <summary>
+        /// 1フレームあたりどのくらい輝度が変わるか。
+        /// 輝度の範囲は[0, 1]
+        /// </summary>
+        [SerializeField]
+        float _speed = 0;
+
+        private Text _text;
+        private float _brightness;
+        private bool _increase;
+
+        void Start()
+        {
+            _text = GetComponent<Text>();
+        }
+
+        void Update()
+        {
+            if (_increase)
+            {
+                _brightness += _speed;
+                if (_brightness > 1)
+                {
+                    _brightness = 1;
+                    _increase = false;
+                }
+            }
+            else
+            {
+                _brightness -= _speed;
+                if (_brightness < 0)
+                {
+                    _brightness = 0;
+                    _increase = transform;
+                }
+            }
+
+            _text.color = new Color(_brightness, _brightness, _brightness, 1);
+        }
+    }
+}

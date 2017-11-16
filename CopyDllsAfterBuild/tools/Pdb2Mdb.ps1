5abfbc3bdb4df073939ad34adf7be50a3b59954a
Nobuyuki Iwanaga <ufcpp@live.jp>
2017-11-16 16:46:29 +0900

    make Pdb2Mdb support VS 15

--- CopyDllsAfterBuild/tools/Pdb2Mdb.ps1
+++ CopyDllsAfterBuild/tools/Pdb2Mdb.ps1
@@ -1,8 +1,30 @@
-$script:vstools = if ($env:VS140COMNTOOLS -ne $null) { $env:VS140COMNTOOLS } `
-    elseif ($env:VS120COMNTOOLS -ne $null) { $env:VS120COMNTOOLS } `
-    elseif ($env:VS110COMNTOOLS -ne $null) { $env:VS110COMNTOOLS }
+﻿# Program Files (x86) があればそれを、なければ Program Files を使う
+if (Test-Path ${env:ProgramFiles(x86)})
+{
+    $Script:ProgramFiles = ${env:ProgramFiles(x86)}
+}
+else
+{
+    $Script:ProgramFiles = $env:ProgramFiles
+}
 
-$script:XABuildTask = [io.path]::Combine($script:vstools, '../../../msbuild/Xamarin/Android/Xamarin.Android.Build.Tasks.dll')
+# 候補。上から順に調べて、最初に見つけたやつを返す
+$Script:items =
+    # VS 2017
+    # side by side インストールできるようになったせいで何か所か探さないとダメ
+    '\Microsoft Visual Studio\2017\Community\',
+    '\Microsoft Visual Studio\2017\Professional\',
+    '\Microsoft Visual Studio\2017\Enterprise\',
+    '\Microsoft Visual Studio\2017\BuildTools\',
+    # VS 2015, 2013
+    $env:VS140COMNTOOLS + '../../../',
+    $env:VS120COMNTOOLS + '../../../'
+
+foreach ($Script:item in $Script:items)
+{
+    $script:XABuildTask = $Script:ProgramFiles + $Script:item + 'msbuild/Xamarin/Android/Xamarin.Android.Build.Tasks.dll'
+    if (Test-Path $script:XABuildTask) { break; }
+}
 
 if (Test-Path $script:XABuildTask)
 {

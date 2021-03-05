13f459205c5d4450ba9bd0aa8a92a0c061d8dd36
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2021-03-05 16:25:32 +0900

    feat: add FileWriter Test

--- CopyDllsAfterBuildLocalTool/CopyDllsAfterBuildLocalToolUnitTest/FileWriterTest.cs
+++ CopyDllsAfterBuildLocalTool/CopyDllsAfterBuildLocalToolUnitTest/FileWriterTest.cs
@@ -2,17 +2,86 @@
 using System;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace CopyDllsAfterBuildLocalToolUnitTest
 {
-    public class FileWriterTest
+    public class FileWriterTest : IDisposable
     {
-        // todo: add Binary Compare test
-        // todo: add Write test. Binary match file should skipped.
-        // todo: add Write test. Binary mismatch file should write.
-        // todo: add Write test. None CheckOption should write even if binary equals.
-        // todo: add Write test. None CheckOption should write if binary mismatch..
+        private readonly string _path;
+
+        // setup
+        public FileWriterTest()
+        {
+            _path = Path.Combine(Path.GetTempPath(), "FileWriterTest", Guid.NewGuid().ToString());
+
+            if (!Directory.Exists(_path))
+                Directory.CreateDirectory(_path);
+        }
+
+        // teardown
+        public void Dispose()
+        {
+            if (Directory.Exists(_path))
+                Directory.Delete(_path, true);
+        }
+
+        [Fact]
+        public void BinaryCompareTest()
+        {
+            var random = new Random();
+            var content = Enumerable.Range(0, 10).Select(x => (byte)random.Next(0, 254)).ToArray();
+            var mismatchContent = content.Append((byte)random.Next(0, 254)).ToArray();
+
+            // binary match item should be true
+            var a = Path.Combine(_path, "a");
+            File.WriteAllBytes(a, content);
+            using var fa = File.OpenRead(a);
+            Assert.True(FileWriter.Compare(content, fa));
+
+            // binary miss-match item should be false
+            var b = Path.Combine(_path, "b");
+            File.WriteAllBytes(b, mismatchContent);
+            using var fb = File.OpenRead(b);
+            Assert.False(FileWriter.Compare(content, fb));
+        }
+
+        [Fact]
+        public void Write_WriteCheckOption_BinaryEquality_Test()
+        {
+            var option = WriteCheckOption.BinaryEquality;
+
+            // Destination not exists should write.
+            var random = new Random();
+            var content = Enumerable.Range(0, 10).Select(x => (byte)random.Next(0, 254)).ToArray();
+            var a = Path.Combine(_path, "a");
+            Assert.True(FileWriter.Write(content, a, option));
+
+            // Binary match file should skipped.
+            Assert.False(FileWriter.Write(content, a, option));
+
+            // Binary mismatch file should write.
+            var mismatchContent = content.Append((byte)random.Next(0, 254)).ToArray();
+            Assert.True(FileWriter.Write(mismatchContent, a, option));
+        }
+
+        [Fact]
+        public void Write_WriteCheckOption_None_Test()
+        {
+            var option = WriteCheckOption.None;
+
+            // Destination not exists should write.
+            var random = new Random();
+            var content = Enumerable.Range(0, 10).Select(x => (byte)random.Next(0, 254)).ToArray();
+            var a = Path.Combine(_path, "a");
+            Assert.True(FileWriter.Write(content, a, option));
+
+            // Binary match file should write.
+            Assert.True(FileWriter.Write(content, a, option));
+
+            // Binary mismatch file should write.
+            var mismatchContent = content.Append((byte)random.Next(0, 254)).ToArray();
+            Assert.True(FileWriter.Write(mismatchContent, a, option));
+        }
     }
 }
